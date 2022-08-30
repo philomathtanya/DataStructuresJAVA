6fa91fb434a45b58ff336ae5cf8c8290ddff3300
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-08-30 16:23:58 +0530

    Find_Words_That_Can_Be_Formed_by_Characters

---
+++ Find_Words_That_Can_Be_Formed_by_Characters.java
@@ -0,0 +1,30 @@
+public class Find_Words_That_Can_Be_Formed_by_Characters {
+    public static int countCharacters(String[] words, String chars) {
+int c=0;
+int sum=0;
+String chars1=chars;
+        for (String s : words) {
+            c=0;
+
+chars=chars1;
+for (int i = 0; i < s.length(); i++) {
+//                int n=0;
+//                n = chars.indexOf(s.charAt(i));
+                if(!chars.contains(String.valueOf(s.charAt(i))))
+                    break;
+                else {
+                    c++;
+                    chars=chars.replaceFirst(String.valueOf(s.charAt(i)),"");
+                }
+            }
+            if(c==s.length())
+                sum+=s.length();
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        String words[] = {"dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin","ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb","ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl","boygirdlggnh","xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx","nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop","hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx","juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr","lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo","oxgaskztzroxuntiwlfyufddl","tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp","qnagrpfzlyrouolqquytwnwnsqnmuzphne","eeilfdaookieawrrbvtnqfzcricvhpiv","sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz","yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue","hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv","cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo","teyygdmmyadppuopvqdodaczob","qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs","qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"};
+        String chars = "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp";
+        System.out.println(countCharacters(words,chars));
+    }
+}
