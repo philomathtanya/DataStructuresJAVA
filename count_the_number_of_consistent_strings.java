0a61c9b4911955da96e4f3f33c4e68ce548c51f4
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-09-23 21:32:39 +0530

    count_the_number_of_consistent_strings

--- count_the_number_of_consistent_strings.java
+++ count_the_number_of_consistent_strings.java
@@ -2,18 +2,22 @@ import java.util.Arrays;
 
 public class count_the_number_of_consistent_strings {
     public static void main(String[] args) {
-        String allowed = "abc", words[] = {"a","b","c","ab","ac","bc","abc"};
+        String allowed = "cad", words[] = {"cc","acd","b","ba","bac","bad","ac","d"};
         System.out.println(countConsistentStrings(allowed,words));
     }
     public static int countConsistentStrings(String allowed, String[] words) {
         int count=0;
-        char string[]=allowed.toCharArray();
-        for (int i = 0; i < allowed.length(); i++) {
-            for (int j = 0; j < words.length; j++) {
-                if(words[i].contains("ab")){
-
+//        char string[]=allowed.toCharArray();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < allowed.length(); j++) {
+                String s=""+allowed.charAt(j);
+                if(words[i].contains(s)){
+                    words[i]=words[i].replaceAll(s,"");
                 }
+                s="";
             }
+            if(words[i].equals(""))
+                count++;
         }
         return count;
     }
