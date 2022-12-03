13fe60e1ded05430d03f5b898b373505aaff6db9
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-12-03 12:19:42 +0530

    Create sumof_subaaray_ranges.java

---
+++ sumof_subaaray_ranges.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        long ans = 0;
+        for(int i=0;i<nums.length;i++){
+            int maxi = nums[i],mini = nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                mini = Math.min(mini,nums[j]);
+                maxi = Math.max(maxi,nums[j]);
+                ans = ans + (maxi-mini);
+            }
+        }
+        return ans;
+    }
+}
